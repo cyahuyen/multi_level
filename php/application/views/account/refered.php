ab654731e233cc26b1cee4f651fe3abc03e6a58a
admin <admin@thangph-cya-PC>
2013-05-04 17:30:47 +0700

    Ngoc LC  Update 17h00

--- php/application/views/account/refered.php
+++ php/application/views/account/refered.php
@@ -4,7 +4,7 @@
             <h1>Refered Members</h1>
         </div>
         <div style="padding:5px 25px;float: left;">
-            <h4>Total Refered members : <?php echo count($refereds);?> Total Refered Balance : 30$</h4>
+            <h4>Total members : <?php echo count($refereds);?> (30$)</h4>
         </div>
     </div>
     <div class="content-body">
