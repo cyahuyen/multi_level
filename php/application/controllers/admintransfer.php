0cb78ccbf0422a759993bf36f34bbf1e36669751
ngoalongkt <ngoalongkt@ngoalongkt-PC>
2013-05-09 09:15:31 +0700

    staff

--- php/application/controllers/admintransfer.php
+++ php/application/controllers/admintransfer.php
@@ -6,21 +6,13 @@ if (!defined('BASEPATH'))
 class Admintransfer extends MY_Controller {
 
     private $data;
-    var $menu_config_0 = array('', '', '', '', '', '');
-    var $menu_config_1 = array('active', '', '', '', '', '');
-    var $menu_config_2 = array('', 'active', '', '', '', '');
-    var $menu_config_3 = array('', '', 'active', '', '', '');
-    var $menu_config_4 = array('', '', '', 'active', '', '');
-    var $menu_config_5 = array('', '', '', '', 'active', '');
-    var $menu_config_6 = array('', '', '', '', '', 'active');
-    var $menu_config_7 = array('', '', '', '', '', '', 'active');
     var $navstack = null;
     var $usertype = array('0' => 'Member', '1' => 'Silver', '2' => 'Gold');
 
     public function __construct() {
         parent::__construct();
         $this->load->model('transaction_model', 'transfer');
-        $this->data['menu_config'] = $this->menu_config_7;
+        $this->data['menu_config'] = $this->menu_config_6;
         $user_session = $this->session->userdata('user');
         if (empty($user_session) || $user_session['permission'] != 'administrator') {
             redirect(site_url('home'));
--- php/application/views/admin_includes/top_menu.php
+++ php/application/views/admin_includes/top_menu.php
@@ -44,7 +44,7 @@
             <li class="<?php echo $menu_config[4] ?>">
                 <a href="<?php echo site_url('adminreport') ?>">Report</a>
             </li>
-            <li><a href="<?php echo site_url('admintransfer') ?>">Transaction</a></li>
+            <li class="<?php echo $menu_config[5] ?>"><a href="<?php echo site_url('admintransfer') ?>">Transaction</a></li>
         </ul>
         <div class="clearer"></div>
     </div>
